b37cca61085148295a19d7b883fa6df6c0ae40dc
Filip Tepper <filip@tepper.pl>
2011-12-15 14:18:10 +0100

    added :add_empty option

--- lib/state_select.rb
+++ lib/state_select.rb
@@ -2,21 +2,33 @@
 
 module ActionView::Helpers::FormOptionsHelper
 
-  # Return select and option tags for the given object and method, using state_options_for_select to generate the list of option tags.
-  def state_select(object, method, country='US', options = {}, html_options = {})
-    ActionView::Helpers::InstanceTag.new(object, method, self, options.delete(:object)).to_state_select_tag(country, options, html_options)
+  # Return select and option tags for the given object and method,
+  # using state_options_for_select to generate the list of option tags.
+
+  def state_select(object, method, country = "US", options = {}, html_options = {})
+    ActionView::Helpers::InstanceTag.new(
+      object,
+      method,
+      self,
+      options.delete(:object)
+    ).to_state_select_tag(country, options, html_options)
   end
 
-  # Returns a string of option tags for states in a country. Supply a state name as +selected+ to
-  # have it marked as the selected option tag.
+  # Returns a string of option tags for states in a country. Supply a state
+  # name as +selected+ to have it marked as the selected option tag.
   #
   # NOTE: Only the option tags are returned, you have to wrap this call in a regular HTML select tag.
 
-  def state_options_for_select(selected = nil, country = 'US')
+  def state_options_for_select(selected = nil, country = "US", options = {})
     state_options = ""
+
     if country
-      state_options += options_for_select(eval(country.upcase+'_STATES'), selected)
+      countries = eval(country.upcase + "_STATES").dup
+
+      countries.unshift("") if options[:add_empty]
+      state_options += options_for_select(countries, selected)
     end
+
     return state_options
   end
 
@@ -143,21 +155,17 @@ module ActionView::Helpers::FormOptionsHelper
 end
 
 class ActionView::Helpers::InstanceTag
-
-
   def to_state_select_tag(country, options, html_options)
     html_options = html_options.stringify_keys
     add_default_name_and_id(html_options)
     value = value(object)
     selected_value = options.has_key?(:selected) ? options[:selected] : value
-    content_tag("select", add_options(state_options_for_select(selected_value, country), options, value), html_options)
+    content_tag("select", add_options(state_options_for_select(selected_value, country, options), options, value), html_options)
   end
 end
-
 
 class ActionView::Helpers::FormBuilder
   def state_select(method, country = 'US', options = {}, html_options = {})
     @template.state_select(@object_name, method, country, options.merge(:object => @object), html_options)
   end
 end
-
